f1ce23f290856bfff9104710d8f450beb399f96b
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2026-03-24 16:36:47 -0700

    fugitive dd, :Gdiff, :Gdiffsplit now show the unfolded diff, while fugitive dv and dh still show the folded diff

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.opt.diffopt:append("vertical")
+
+vim.api.nvim_create_user_command("Gdiffsplit", function(opts)
+    vim.cmd("Gvdiffsplit" .. (opts.bang and "!" or "") .. " " .. opts.args)
+    vim.cmd("windo normal! zR")
+end, { nargs = "?", bang = true })
+
+vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
